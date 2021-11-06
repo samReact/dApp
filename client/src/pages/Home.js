7f68f5dc9be32cd628b6dbe8e72fa45809922dbf
samReact <sam.react001@gmail.com>
2021-11-06 21:58:25 +0100

    add particles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,31 +1,5 @@
-import { Container } from 'react-bootstrap'
+import ParticlesComponent from '../components/Particles'
 
-import NewZombieForm from '../parts/NewZombieForm'
-
-const Home = ({
-  accounts,
-  balance,
-  levelUpFee,
-  zombies,
-  handleChange,
-  handleSubmit,
-}) => {
-  return (
-    <>
-      <h2>Smart Contract Example</h2>
-      <div>Account: {accounts[0]}</div>
-      <div>Balance: {balance} ETH</div>
-      <div>LevelUp Fee: {levelUpFee} ETH</div>
-      <Container>
-        {zombies.length === 0 && (
-          <NewZombieForm
-            handleSubmit={handleSubmit}
-            handleChange={handleChange}
-          />
-        )}
-      </Container>
-    </>
-  )
-}
+const Home = () => <ParticlesComponent />
 
 export default Home
